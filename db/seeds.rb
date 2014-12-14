1542fc1ae3218895011c874849c645bf601c2738
Ronald Hong <rhong@fiksu.com>
2014-12-13 20:14:06 -0500

    Seed a sample CV to cvs table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+current_cv = Cv.new(value: File.read('db/flat/resume.json'))
+current_cv.is_current = true
+current_cv.save!
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
