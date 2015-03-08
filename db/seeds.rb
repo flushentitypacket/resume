5cb67752b69df74322fc4c165af8d9afd5d81465
Ronald Hong <ronniejhong@gmail.com>
2015-03-08 16:14:48 -0400

    Update db seed to update existing records, if any.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 resume = JSON.parse(File.read('db/flat/resume.json'))
-Cv.create!(value: resume, version: resume['meta']['version'])
+Cv.first_or_initialize(version: resume['meta']['version']).tap do |cv|
+  cv.value = resume
+end.save
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
